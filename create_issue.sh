c595217bb4521eb107fafa9fdb4562b79332c620
athorat940 <92424910+athorat940@users.noreply.github.com>
2021-12-15 16:09:10 +0530

    Create create_issue.sh

---
+++ create_issue.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+cd /opt/test
+git clone https://github.com/athorat940/jenkins-pipeline-tutorial.git
+cd /opt/test/jenkins-pipeline-tutorial
+commit=$(git log |  awk '{print $2}' | head -n1)
+echo $commit
+gh issue create --title "Build Failure" --body "Root cause : $commit" --label "Incident"
