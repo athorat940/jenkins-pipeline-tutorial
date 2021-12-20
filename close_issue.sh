899863e4ecee40e9db18101d6bbd448f8b943346
athorat940 <92424910+athorat940@users.noreply.github.com>
2021-12-20 18:09:20 +0530

    Update close_issue.sh

--- close_issue.sh
+++ close_issue.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+jenkins-pipeline-tutorial
 git clone https://github.com/athorat940/jenkins-pipeline-tutorial.git
 cd jenkins-pipeline-tutorial
 data=$(gh issue list | awk '{print $1}' | head -n1)
