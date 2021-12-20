ad28c629bbaf89afa29ab67c2f29e366bc8f9d46
athorat940 <92424910+athorat940@users.noreply.github.com>
2021-12-20 18:13:42 +0530

    Update create_issue.sh

--- create_issue.sh
+++ create_issue.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-rm -rf jenkins-pipeline-tutorial
-git clone https://github.com/athorat940/jenkins-pipeline-tutorial.git
 cd jenkins-pipeline-tutorial
 commit=$(git log |  awk '{print $2}' | head -n1)
 echo $commit
